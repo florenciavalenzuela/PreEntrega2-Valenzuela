fa7e592dc5a90ed3ca3d1f1290b396afdbe793f8
Flor Valenzuela <floor.valenzuela@hotmail.com>
2023-08-24 23:14:53 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+
+const App = () => {
+
+  return (
+    <>
+      <navBar/>
+    </> 
+  
+  )
+}
+
+export default App
